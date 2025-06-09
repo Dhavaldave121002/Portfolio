dee850d1f4c8e65919fb7bea1e240989dc631bf5
Dhavaldave121002 <dhavaldave121002@gmail.com>
2025-06-09 16:10:08 +0530

    Portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,95 @@
-console.log("Hello World");
+// window.addEventListener("load", () => {
+//   // Logo bounce + glow pulse
+//   gsap.fromTo("#loader-logo", {
+//     scale: 0.5,
+//     opacity: 0
+//   }, {
+//     scale: 1,
+//     opacity: 1,
+//     duration: 1,
+//     rotation: 360,
+//     ease: "bounce.out"
+//   });
+
+//   gsap.to("#loader-logo", {
+//     repeat: -1,
+//     yoyo: true,
+//     duration: 1.2,
+//     scale: 1.05,
+//     ease: "sine.inOut"
+//   });
+
+//   // Text fade + pulse
+//   gsap.from("#loader-text", {
+//     opacity: 0,
+//     y: 20,
+//     delay: 0.5,
+//     duration: 1.2,
+//     ease: "power2.out"
+//   });
+
+//   // Hide loader and reveal main content
+//   setTimeout(() => {
+//     gsap.to("#loader", {
+//       opacity: 0,
+//       duration: 1,
+//       onComplete: () => {
+//         document.getElementById("loader").style.display = "none";
+//         document.getElementById("main-content").style.display = "block";
+
+//         // Animate main content
+//         gsap.from("header", {
+//           opacity: 0,
+//           y: -50,
+//           duration: 1,
+//           ease: "power2.out"
+//         });
+//       }
+//     });
+//   }, 3000); // Wait 3 seconds
+// });
+
+var log = document.querySelector(".logo");
+var list = document.querySelector(".list");
+var menu = document.querySelector(".list i");
+var tl = gsap.timeline();
+
+tl.from(log, {
+  opacity: 0,
+  y: -80,
+  scale: 0.5,
+  duration: 1,
+  ease: "bounce.out"
+});
+
+tl.from(list, {
+  opacity: 0,
+  x: -80,
+  scale: 0.5,
+  duration: 1,
+  stagger: 0.5,
+  ease: "back.out(1.5)"
+});
+var tl1 = gsap.timeline();
+
+tl1.from(menu, {
+  opacity: 0,
+  x: -10,
+  duration: 0.5,
+  ease: "back.out(1.5)"
+});
+menu.addEventListener("click", () => {
+  list.classList.toggle("show");
+
+  if (list.classList.contains("show")) {
+    gsap.from(".list h4", {
+      opacity: 0,
+      x: 30,
+      duration: 0.4,
+      stagger: 0.1,
+      ease: "power1.out"
+    });
+  }
+});
+
+
